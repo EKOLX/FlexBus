3bd7f0845f9679f0cdae54cf2698b3de5b44ef21
Elkhan <elxanmursal@gmail.com>
2020-01-15 22:10:59 +0100

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { AppComponent } from "./app.component";
 import { LinkedinSvg } from "./svg-assets/in-logo.svg";
 import { BusSvg } from "./svg-assets/bus.svg";
 import { ArrowSvg } from "./svg-assets/arrow.svg";
+import { HomeComponent } from './home/home.component';
+import { StationListComponent } from './station-list/station-list.component';
+import { BusListComponent } from './bus-list/bus-list.component';
 
 @NgModule({
-  declarations: [AppComponent, LinkedinSvg, BusSvg, ArrowSvg],
+  declarations: [AppComponent, LinkedinSvg, BusSvg, ArrowSvg, HomeComponent, StationListComponent, BusListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
